3b349dc835066ec8e29aa65735e68b05f90d92f9
Devika Patadia <devikapatadia9@Devikas-MacBook-Air.local>
2023-02-04 19:15:43 -0800

    Initial Commit

---
+++ Assets/ballmovementscript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmovementscript : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+
+    private Rigidbody2D rigidBody;
+    private Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        direction = Random.insideUnitCircle.normalized;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(horizontal, vertical);
+        rigidBody.velocity = direction * speed;
+        
+    }
+}
