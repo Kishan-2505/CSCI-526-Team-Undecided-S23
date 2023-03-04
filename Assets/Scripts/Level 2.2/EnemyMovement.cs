7b972119a34807c3e3e64dd871cd5f511cddf6b8
rushankshah <rushankshah65@gmail.com>
2023-03-04 15:23:04 -0800

    Level 2 commit

---
+++ Assets/Scripts/Level 2.2/EnemyMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level2_2
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        public GameOverScreen gameOverScreen;
+        public ballmovementscript ballmovementscript;
+
+        private int health = 100;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        public GameObject player;
+        public float speed = 2.0f;
+        private float elapsedTime;
+
+
+        void Update()
+        {
+            if (!ballmovementscript.isBallFrozen)
+            {
+                Vector3 targetPosition = player.transform.position;
+                Vector3 enemyPosition = transform.position;
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(enemyPosition, targetPosition, step);
+            }
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+                elapsedTime = Time.time - ballmovementscript.startTime;
+                gameOverScreen.Setup(ballmovementscript.score, elapsedTime, 3, "You Won!", ballmovementscript.bulletsFired, ballmovementscript.bulletHit, ballmovementscript.isGettingSmall); // 3 is win state
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Spike")
+            {
+                Destroy(gameObject);
+                Destroy(collision.gameObject);
+                Debug.Log("You Won!");
+                elapsedTime = Time.time - ballmovementscript.startTime;
+                gameOverScreen.Setup(ballmovementscript.score, elapsedTime, 3, "You Won!", ballmovementscript.bulletsFired, ballmovementscript.bulletHit, ballmovementscript.isGettingSmall); // 3 is win state
+            }
+            if (collision.gameObject.tag == "Bullet")
+            {
+                // Destroy(gameObject);
+                ballmovementscript.bulletHit += 1;
+                Destroy(collision.gameObject);
+                health -= 25; // 3 is win state
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Level 2.2/FreezeFoodGenerator.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Level2_2
+{
+    public class FreezeFoodGenerator : MonoBehaviour
+    {
+        public GameObject trianglePrefab;
+        // public float timeInterval = 5.0f;
+        public float timeInterval = 20.0f;
+        private float timeCounter = 0.0f;
+
+        private void Update()
+        {
+            timeCounter += Time.deltaTime;
+            if (timeCounter >= timeInterval)
+            {
+                timeCounter = 0.0f;
+                trianglePrefab.tag = "FreezeFood";
+                Vector3 randomPosition = new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(-4.0f, 4.0f), 0.0f);
+                Instantiate(trianglePrefab, randomPosition, Quaternion.identity);
+            }
+        }
+
+    }
+}
