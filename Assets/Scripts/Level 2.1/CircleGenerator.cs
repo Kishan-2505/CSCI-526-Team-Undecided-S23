1b8c3d3d287b52b408d68c0c8ed8652c4f18da03
Kishan Rathod <kishanrathodgdd@gmail.com>
2023-03-04 17:12:17 -0800

    Added level2.1

---
+++ Assets/Scripts/Level 2.1/CircleGenerator.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Level2_1
+{
+
+    public class CircleGenerator : MonoBehaviour
+    {
+        public GameObject circlePrefab;
+        public float timeInterval = 2.0f;
+        private float timeCounter = 0.0f;
+
+        private void Update()
+        {
+            timeCounter += Time.deltaTime;
+            if (timeCounter >= timeInterval)
+            {
+                timeCounter = 0.0f;
+                circlePrefab.tag = "food";
+                Vector3 randomPosition = new Vector3(Random.Range(-8.0f, 5.0f), Random.Range(-4.0f, 4.0f), 0.0f);
+                Instantiate(circlePrefab, randomPosition, Quaternion.identity);
+            }
+        }
+
+    }
+}
