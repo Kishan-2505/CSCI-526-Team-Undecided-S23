76e7fca01884689db7b03adffdfdf40574516770
RiddhamGadia <rgadia2000@gmail.com>
2023-03-26 17:13:35 -0700

    Implemented level 2 without art

---
+++ Assets/Scripts/Level 3_2/EnemyMovement2.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level3_2
+{
+    public class EnemyMovement2 : MonoBehaviour
+    {
+        // public GameOverScreen gameOverScreen;
+        // public ballmovementscript ballmovementscript;
+
+        public PlayerMovement playerMovement;
+        public GameObject player;
+        public float speed = 2.0f;
+        private float elapsedTime;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame       
+        void Update()
+        {
+           if (!playerMovement.isEnemy2Freeze)
+            {
+                Vector3 targetPosition = player.transform.position;
+                Vector3 enemyPosition = transform.position;
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(enemyPosition, targetPosition, step);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Level 3_2/PlayerMovement.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+namespace Level3_2
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        private float speed = 8.0f;
+        private Rigidbody2D rigidBody;
+        private float timeInterval = 1.0f;
+        private float timeCounter = 0.0f;
+        private int diamondCount = 0;
+        public TextMeshProUGUI diamondText;
+        public GameOverScript gameOverScript;
+        private GameObject inGameCanvas;
+
+        public bool isEnemy1Freeze = true;
+        public bool isEnemy2Freeze = true;
+        // Start is called before the first frame update
+        void Start()
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+            inGameCanvas = GameObject.Find("In Game Canvas");
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            Vector2 direction = new Vector2(horizontal, vertical);
+            rigidBody.velocity = direction * speed;
+        }
+
+        private void Update()
+        {
+            gettingSmall();
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            Bounds bounds = renderer.bounds;
+            Vector2 size = bounds.size;
+            if (size.x <= 0.3f || size.y <= 0.3f)
+            {
+                gameOverScript.Setup("You died!");
+                inGameCanvas.SetActive(false);
+            }
+        }
+
+        private void gettingSmall()
+        {
+            timeCounter += Time.deltaTime;
+            if (timeCounter >= timeInterval)
+            {
+                transform.localScale += new Vector3(-0.03f, -0.03f, 0);
+                timeCounter = 0.0f;
+            }
+        }
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Diamond"))
+            {
+                Destroy(collision.gameObject);
+                diamondCount++;
+                diamondText.text = "Diamonds: " + diamondCount + "/3"; 
+            }
+            if (collision.gameObject.CompareTag("Door"))
+            {
+                if(diamondCount==3)
+                {
+                    gameOverScript.Setup("You won!");
+                    inGameCanvas.SetActive(false);
+                }
+                else
+                {
+                    Debug.Log("Collect all 3 diamonds");
+                }
+            }
+            if (collision.gameObject.CompareTag("enemy1"))
+            {
+                SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+                Bounds bounds = renderer.bounds;
+                Vector2 size = bounds.size;
+                if (size.x>=0.8f)
+                {
+                    Destroy(collision.gameObject);
+                }
+                else
+                {
+                    Debug.Log("You collided with an enemy");
+                }
+
+            }
+            if (collision.gameObject.CompareTag("enemy2"))
+            {
+                SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+                Bounds bounds = renderer.bounds;
+                Vector2 size = bounds.size;
+                if (size.x>=0.8f)
+                {
+                    Destroy(collision.gameObject);
+                }
+                else
+                {
+                    Debug.Log("You collided with an enemy");
+                }
+
+            }
+            if(collision.gameObject.CompareTag("Enemy1Detector"))
+            {
+                isEnemy1Freeze = false;
+                Destroy(collision.gameObject);
+            }
+            if(collision.gameObject.CompareTag("Enemy2Detector"))
+            {
+                isEnemy2Freeze = false;
+                Destroy(collision.gameObject);
+            }
+        }
+        // public void RestartButton()
+        // {
+        //     Time.timeScale = 1;
+        //     SceneManager.LoadScene("Level 3_2");
+        // }
+        // public void MainMenuButton()
+        // {
+        //     Time.timeScale = 1;
+        //     SceneManager.LoadScene("Level Selector");
+        // }
+    }
+}
+
