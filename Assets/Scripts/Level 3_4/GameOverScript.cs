0e2f0a8739984c74b22796517b4c56c8aa73c409
NemilShah007 <shahnemil103@gmail.com>
2023-04-04 02:38:55 -0700

    Modified Level 4 functionality

---
+++ Assets/Scripts/Level 3_4/GameOverScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+namespace Level3_4
+{
+    public class GameOverScript : MonoBehaviour
+    {
+        public TextMeshProUGUI gameOverText; 
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        public void Setup(string message)
+        {
+            gameObject.SetActive(true);
+            gameOverText.text = message;
+        }
+        public void RestartButton()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("Level 3_3");
+        }
+        public void MainMenuButton()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("Level Selector");
+        }
+    }
+}
