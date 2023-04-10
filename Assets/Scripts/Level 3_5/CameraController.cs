f6e887c50a01aed7319b0ce015b8b93acb8c39b5
NemilShah007 <shahnemil103@gmail.com>
2023-04-09 19:34:44 -0700

    Implemented Level 5

---
+++ Assets/Scripts/Level 3_5/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace level3_5
+{
+    public class CameraController : MonoBehaviour
+    {
+        private Camera Camera;
+        private float zoomOut = 20f;
+        private const float zoomIn = 12.14533f; // Const do not change it
+        void Start()
+        {
+            Camera = GetComponent<Camera>();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                Camera.orthographicSize = zoomOut;
+            }
+            else if (Input.GetKeyUp(KeyCode.M))
+            {
+                Camera.orthographicSize = zoomIn;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Level 3_5/GameOverScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+namespace Level3_5
+{
+    public class GameOverScript : MonoBehaviour
+    {
+        public TextMeshProUGUI gameOverText;
+        public GameObject restartButton;
+        public GameObject nextLevelButton;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        public void Setup(string message)
+        {
+            gameObject.SetActive(true);
+            gameOverText.text = message;
+            if (message == "You died!" || message == "Enemy ate you!")
+            {
+                restartButton.SetActive(true);
+                nextLevelButton.SetActive(false);
+            }
+            if (message == "You won!")
+            {
+                restartButton.SetActive(false);
+                nextLevelButton.SetActive(true);
+                nextLevelButton.transform.position = restartButton.transform.position;
+            }
+        }
+        public void RestartButton()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("Level 3_4");
+        }
+        public void MainMenuButton()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("Level Selector");
+        }
+        public void NextLevelButton()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("Level 3_5");
+        }
+    }
+}
